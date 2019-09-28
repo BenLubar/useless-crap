75c7fc65e227803537bb40358402b5c723b5371d
Ben Lubar <ben.lubar@gmail.com>
2019-09-27 20:22:48 -0500

    add vtt

---
+++ vtt.js
@@ -0,0 +1,56 @@
+(function() {
+	"use strict";
+
+	var style = document.createElement("style");
+	document.body.appendChild(style);
+	style.sheet.insertRule('::cue(.hidden) { visibility: hidden; }');
+	var karaoke = /<(?:(?:([0-9]{2,}):)?([0-5][0-9]):)?([0-5][0-9]\.[0-9]{3})>/g;
+	document.addEventListener("load", function(e) {
+		var el = e.target;
+		if (el.nodeType !== Node.ELEMENT_NODE || el.nodeName !== "TRACK") {
+			return;
+		}
+
+		var track = el.track;
+		[].forEach.call(track.cues, function(cue) {
+			var parts = cue.text.split(karaoke);
+			if (parts.length <= 1) {
+				return;
+			}
+	
+			var timestamps = [cue.startTime, cue.endTime];
+			for (var i = 1; i < parts.length; i += 2) {
+				var ts = parts.splice(i, 3, 0);
+				var hrs = parseInt(ts[0] || "0", 10);
+				var min = parseInt(ts[1] || "0", 10);
+				var sec = parseFloat(ts[2]);
+				parts[i] = (hrs * 60 + min) * 60 + sec;
+				timestamps.push(parts[i]);
+			}
+	
+			track.removeCue(cue);
+	
+			timestamps.sort(function(a, b) {
+				return a - b;
+			}).filter(function(v, i, a) {
+				return a.indexOf(v) === i;
+			});
+	
+			for (var i = 0; i < timestamps.length - 1; i++) {
+				var start = timestamps[i];
+				var end = timestamps[i + 1];
+				var text = [parts[0]];
+				for (var j = 1; j < parts.length; j += 2) {
+					if (parts[j] <= start) {
+						text.push(parts[j + 1]);
+					} else {
+						text.push("<c.hidden>");
+						text.push(parts[j + 1]);
+						text.push("</c>");
+					}
+				}
+				track.addCue(new VTTCue(start, end, text.join("")));
+			}
+		});
+	}, true);
+})();
